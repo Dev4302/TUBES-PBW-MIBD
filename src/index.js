2d2973922ed0d03abbb34f8509e6efdcb6fd1328
Devlin <devlin.venedict@gmail.com>
2023-06-11 14:50:23 +0700

    post berhasil

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('CSS'));
+app.use(express.static('Script'));
+app.use(express.urlencoded({extended:true}));
+
 app.set('view engine', 'ejs');
 const port = 8080;
 
@@ -10,19 +14,36 @@ app.get('/' , async(req,res) =>
     res.render('Face1');
 });
 
+
 app.get('/aboutus', async(req,res)=>
 {
     res.render('AboutUs!');
 });
 
+
 app.get('/daftarPengajar', async(req,res)=>
 {
     res.render('daftarPengajar');
 });
+app.post('/daftarPengajar',async (req, res)=>{
+
+    const data = req.body.username;
+    console.log(data);
+   
+    res.redirect('/jadwalPengajar')
+});
+
 
 app.get('/jadwalPengajar', async(req,res)=>
 {
     res.render('jadwalPengajar');
+});
+app.post('/jadwalPengajar',async (req, res)=>{
+
+    const data = req.body.username;
+    console.log(data);
+   
+    res.redirect('/optlogin');
 });
 
 app.get('/Homepage-student', async(req,res)=>
@@ -34,18 +55,24 @@ app.get('/daftarMurid', async(req,res)=>
 {
     res.render('daftarMurid');
 });
-
 app.post('/daftarMurid',async (req, res)=>{
 
-    const data = req.body.username;
+    const data = req.body;
     console.log(data);
    
-    res.redirect('/optlogin')
+    res.redirect('/optlogin');
 });
 
 app.get('/LoginMurid', async(req,res)=>
 {
     res.render('LoginMurid');
+});
+app.post('/LoginMurid',async (req, res)=>{
+
+    const data = req.body.username;
+    console.log(data);
+   
+    res.redirect('/Homepage-student');
 });
 
 app.get('/optlogin', async(req,res)=>
@@ -56,6 +83,13 @@ app.get('/optlogin', async(req,res)=>
 app.get('/LoginPengajar', async(req,res)=>
 {
     res.render('LoginPengajar');
+});
+app.post('/LoginPengajar',async (req, res)=>{
+
+    const data = req.body.username;
+    console.log(data);
+   
+    res.redirect('/Homepage-teacher');
 });
 
 app.get('/RegisterCourse', async(req,res)=>
